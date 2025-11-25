b8c574d004e3d219696dfbb491e45201821abee4
Yasin <yasin.arshad04@gmail.com>
2025-11-25 16:55:19 +0100

    Add loading, error handling and improved title styling

--- app/src/main/java/com/jikananime/app/ui/search/SearchScreen.kt
+++ app/src/main/java/com/jikananime/app/ui/search/SearchScreen.kt
@@ -1,11 +1,123 @@
 package com.jikananime.app.ui.search
 
+import android.R.attr.contentDescription
+import android.R.attr.label
+import android.R.attr.text
+import android.R.id.bold
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.rememberAsyncImagePainter
+import com.jikananime.app.ui.screen.search.SearchViewModel
 
 @Composable
-fun SearchScreen() {
-    Box(modifier = androidx.compose.ui.Modifier.fillMaxSize()) {
+fun SearchScreen(searchViewModel: SearchViewModel = viewModel()) {
+
+    val anime = searchViewModel.anime.collectAsState()
+    val error = searchViewModel.error.collectAsState()
+    val loading = searchViewModel.loading.collectAsState()
+    var id by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Text("Søk etter anime (ID)")
+        TextField(
+            value = id,
+            onValueChange = {id = it},
+            label = {Text("Anime ID")}
+        )
+        OutlinedButton(
+            onClick = {
+                val parsed = id.toIntOrNull()
+                if (parsed != null){
+                    searchViewModel.searchById(parsed)
+                }
+            }
+        ) {
+            Text("Søk")
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        if (loading.value) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Laster...")
+            }
+            return@Column
+        }
+
+        error.value?.let { err->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = err,
+                    color = Red
+
+                )
+            }
+            return@Column
+        }
+
+        anime.value?.let { animeData ->
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = animeData.title,
+                    fontSize = 22.sp,
+                    fontWeight = Bold
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                val imageUrl = animeData.images.jpg.image_url
+
+                Image(
+                    painter = rememberAsyncImagePainter(model = imageUrl),
+                    contentDescription = animeData.title,
+                    modifier = Modifier
+                        .size(250.dp)
+                        .padding(top = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = animeData.synopsis)
+            }
+
+        } ?: Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Ingen anime – skriv ID og søk")
+        }
     }
 }
--- app/src/main/java/com/jikananime/app/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/jikananime/app/ui/search/SearchViewModel.kt
@@ -2,15 +2,44 @@ package com.jikananime.app.ui.screen.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jikananime.app.data.remote.dto.AnimeDetailDto
 import com.jikananime.app.data.repository.JikanRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SearchViewModel : ViewModel() {
 
     private val repository = JikanRepository()
 
+    private val _anime = MutableStateFlow<AnimeDetailDto?>(null)
+    val anime = _anime.asStateFlow()
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error = _error.asStateFlow()
+
+    private val _loading = MutableStateFlow(false)
+    val loading = _loading.asStateFlow()
+
     fun searchById(id: Int) {
         viewModelScope.launch {
+            try {
+                _error.value = null
+                _anime.value = null
+                _loading.value = true
+
+                val response = repository.getAnimeById(id)
+                _anime.value = response.data
+
+                if (response.data == null) {
+                    _error.value = "Fant ingen anime med ID $id"
+                }
+            } catch (e: Exception){
+                e.printStackTrace()
+                _error.value = "Fant ingen anime med ID $id"
+            } finally {
+                _loading.value = false
+            }
         }
     }
 }
